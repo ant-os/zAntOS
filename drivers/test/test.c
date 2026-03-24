1dcc63f46aa144f631b6e607831c45ee1694bdc4
Joscha Egloff <joscha.egloff@pm.me>
2026-03-24 12:59:12 +0000

    add AntkDebugPrintEx

--- drivers/test/test.c
+++ drivers/test/test.c
@@ -3,18 +3,18 @@
 #include "../../include/antk.h"
 #include "../../include/io.h"
 
-ANTKAPI ANTSTATUS HandleRead(PIRP Irp, IRP_PARAMS_READ *Parameters, void *Context);
+ANTKAPI ANTSTATUS HandleWrite(PIRP Irp, IRP_PARAMS_WRITE *Parameters, void *Context);
 
 ANTKAPI ANTSTATUS AntkDriverEntry(PKO_DRIVER DriverObect, void *unused) {
     AntkDebugPrint("test");
 
-    IoInstallHandler(DriverObect, IRP_MJ_READ, HandleRead);
+    IoInstallHandler(DriverObect, IRP_MJ_WRITE, HandleWrite);
 
     return ANTSTATUS_SUCCESS;
 }
 
-ANTKAPI ANTSTATUS HandleRead(PIRP Irp, IRP_PARAMS_READ *Parameters, void *Context) {
-    AntkDebugPrint("handling read irp");
+ANTKAPI ANTSTATUS HandleWrite(PIRP Irp, IRP_PARAMS_WRITE *Parameters, void *Context) {
+    AntkDebugPrintEx("writing \"%s\" at offset %x", Parameters->Buffer, (int)Parameters->Offset);
 
     PIRP_STACK_ENTRY StackEntry = IrpCurrentStackEntry(Irp);
 
